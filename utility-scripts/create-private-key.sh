30ae7d7771ede2311ffb7b9abf86a7cbb4e2d21f
Jonas Lundberg <lundbergjonas96@gmail.com>
2024-07-26 21:40:18 +0000

    Support running the script from another dir

--- utility-scripts/create-private-key.sh
+++ utility-scripts/create-private-key.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+#Get the dir of the script so that keys can be found no matter where the use is running the script from.
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
 # Function to check if a command is available
 if ! command -v age-keygen &> /dev/null
 then
@@ -7,8 +10,8 @@ then
     exit 1
 fi
 
-public_key_file="age_public.txt"
-
+public_key_file="$SCRIPT_DIR/age_public.txt"
+secret_key_file="$SCRIPT_DIR/secrets/age.agekey"
 force_flag=false
 
 # Parse command-line options
@@ -27,18 +30,21 @@ done
 # Check if the age key exists and if the force flag is not set
 if [ ! -e "$public_key_file" ] || [ "$force_flag" = true ]; then
     echo "Age key does not exist or -f flag is provided. Creating it now"
-    rm -f ./secrets/age.agekey $public_key_file
-    age-keygen -o ./secrets/age.agekey
-    age-keygen -y ./secrets/age.agekey > "$public_key_file"
+    rm -f $secret_key_file $public_key_file
+    age-keygen -o $secret_key_file
+    age-keygen -y $secret_key_file > "$public_key_file"
 else
     echo "Public key already exists for the secret. Skipping key generation. Use -f to force new key creation."
 fi
 
-echo "Adding key to cluster"
-    new_secret_yaml=$( cat ./secrets/age.agekey |
-       kubectl create secret generic sops-age \
-                    --namespace=flux-system \
-                    --from-file=age.agekey=/dev/stdin \
-                    --dry-run=client \
-                    -o yaml)
-    echo "$new_secret_yaml" | kubectl apply -f -
+echo "Generating YAML for key"
+new_secret_yaml=$( cat $secret_key_file |
+    kubectl create secret generic sops-age \
+                --namespace=flux-system \
+                --from-file=age.agekey=/dev/stdin \
+                --dry-run=client \
+                -o yaml)
+
+echo "Creating secret in cluster"
+echo "$new_secret_yaml" | kubectl apply -f -
+echo "Done!"
