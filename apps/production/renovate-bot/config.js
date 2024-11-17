30e7dec16d0c52eb107374fdb8f1c2914e39373f
Jonas Lundberg <lundbergjonas96@gmail.com>
2024-11-17 18:40:57 +0000

    Change package rules for linuxserver

--- apps/production/renovate-bot/config.js
+++ apps/production/renovate-bot/config.js
@@ -1,23 +1,32 @@
 module.exports = {
-    autodiscover: true,
-    gitAuthor: "Renovate Bot <bot@renovateapp.com>",
-    platform: "github",
-    requireConfig: "required",
-    onboarding: true,
-    hostRules: [
-        {
-            matchHost: "https://artifactory.local.hejsan.xyz/docker",
-        }
-    ],
-    packageRules: [
-      {
-        "packagePatterns": ["\\/linuxserver\\/"],
-        "versioning": "regex:^(?<compatibility>.*?)-(?<major>v?\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)[\\.-]*r?(?<build>\\d+)*-*r?(?<release>\\w+)*"
-      },
-      {
-        matchUpdateTypes: ["minor", "patch", "pin", "digest"],
-        automerge: true
-      }
-    ],
-    forkProcessing: "enabled"
-}
+  autodiscover: true,
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  platform: "github",
+  requireConfig: "required",
+  onboarding: true,
+  hostRules: [
+    {
+      matchHost: "https://artifactory.local.hejsan.xyz/docker",
+    },
+  ],
+  packageRules: [
+    {
+      packagePatterns: [
+        "^artifactory.local.hejsan.xyz\\/docker\\/linuxserver\\/",
+      ],
+      versioning: `regex:^
+(?<compatibility>version-|v)?
+(?<major>\d+)\.(?<minor>\d+)(\.(?<patch>\d+))?
+(?:
+  [\.-]?
+  (?<build>(\d+-)?(?:ls)?\d+)
+)?
+$`,
+    },
+    {
+      matchUpdateTypes: ["minor", "patch", "pin", "digest"],
+      automerge: true,
+    },
+  ],
+  forkProcessing: "enabled",
+};
