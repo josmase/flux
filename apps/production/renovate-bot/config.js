ea7290513b906893727201bd9f34799a3fa30086
Jonas Lundberg <lundbergjonas96@gmail.com>
2024-11-23 21:20:36 +0000

    Enable more repos and fix pattern for linuxserver

--- apps/production/renovate-bot/config.js
+++ apps/production/renovate-bot/config.js
@@ -2,11 +2,11 @@ module.exports = {
   repositories: [
     "josmase/flux",
     //"josmase/downloader",
-    //"josmase/ansible",
-    //"josmase/bloh",
+    "josmase/ansible",
+    "josmase/blog",
     //"josmase/boplats-map",
-    //"josmase/workflows",
-    //"josmase/devcontainer-templates",
+    "josmase/workflows",
+    "josmase/devcontainer-templates",
   ],
   gitAuthor: "Renovate Bot <bot@renovateapp.com>",
   platform: "github",
@@ -15,16 +15,9 @@ module.exports = {
   packageRules: [
     {
       packagePatterns: [
-        "^artifactory.local.hejsan.xyz\\/docker\\/linuxserver\\/",
+        "^artifactory\\.local\\.hejsan\\.xyz\\/docker\\/linuxserver\\/",
       ],
-      versioning: `regex:^
-(?<compatibility>version-|v)?
-(?<major>\d+)\.(?<minor>\d+)(\.(?<patch>\d+))?
-(?:
-  [\.-]?
-  (?<build>(\d+-)?(?:ls)?\d+)
-)?
-$`,
+      versioning: `regex:^(?<compatibility>version-|v)?(?<major>\\d+)\\.(?<minor>\\d+)(\\.(?<patch>\\d+))?(?:[\\.-]?(?<build>(\\d+-)?(?:ls)?\\d+))?$`,
     },
     {
       matchUpdateTypes: ["minor", "patch", "pin", "digest"],
