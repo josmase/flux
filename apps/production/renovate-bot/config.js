90fd4f2eae8cf6502c1ba5b20d7d7fa8ceed2e95
Jonas Lundberg <lundbergjonas96@gmail.com>
2024-11-30 13:10:04 +0000

    Migrate packagePatterns to matchPackageNames

--- apps/production/renovate-bot/config.js
+++ apps/production/renovate-bot/config.js
@@ -16,15 +16,13 @@ module.exports = {
   labels: ["dependencies", "bot"],
   packageRules: [
     {
-      packagePatterns: [
+      matchPackageNames: [
         "^artifactory\\.local\\.hejsan\\.xyz\\/docker\\/linuxserver\\/",
       ],
-      versioning: `regex:^(?<compatibility>version-|v)?(?<major>\\d+)\\.(?<minor>\\d+)(\\.(?<patch>\\d+))?(?:[\\.-]?(?<build>(\\d+-)?(?:ls)?\\d+))?$`,
+      versioning:
+        "regex:^(?<compatibility>version-|v)?(?<major>\\d+)\\.(?<minor>\\d+)(\\.(?<patch>\\d+))?(?:[\\.-]?(?<build>(\\d+-)?(?:ls)?\\d+))?$",
     },
-    {
-      matchUpdateTypes: ["minor", "patch", "pin", "digest"],
-      automerge: true,
-    },
+    { matchUpdateTypes: ["minor", "patch", "pin", "digest"], automerge: true },
   ],
   forkProcessing: "enabled",
   extends: ["config:best-practices"],
